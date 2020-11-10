44c1f26c580832d07bbcab761b340465f3550fe7
b50905du <71679904+b50905du@users.noreply.github.com>
2020-11-10 17:38:25 +0800

    期中作業

---
+++ Unity_asia_2D_midterm_107051011/Assets/Script/MenuManager.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuManager : MonoBehaviour
+{
+    public void startgame()
+    {
+        print("開始遊戲");
+        SceneManager.LoadScene("關卡一");
+    }
+    public void quitgame()
+    {
+        print("結束遊戲");
+        Application.Quit();
+    }
+}
